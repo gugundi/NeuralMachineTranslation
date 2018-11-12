14bcbc5055245d0c51b0623a5f403b6da97f4d44
Christoffer Øhrstrøm <chrisnave92@gmail.com>
2018-11-12 15:55:49 +0100

    added tensorboardX to install script

--- hpc/install_requirements.sh
+++ hpc/install_requirements.sh
@@ -1,4 +1,3 @@
 conda install pytorch torchvision cuda92 ignite -c pytorch -y
 conda install tensorflow -c anaconda -y
-conda install tensorboard -c conda-forge -y
-pip install --user grpcio msgpack torchtext spacy
+pip install --user grpcio msgpack torchtext spacy tensorboardX
