7a4e22a89da941ca7a84b5dada2eec45cee2e4fb
Christoffer Øhrstrøm <chrisnave92@gmail.com>
2018-11-18 22:28:17 +0100

    updated install requirements to include newest version of tensorflow and tensorboard

--- hpc/install_requirements.sh
+++ hpc/install_requirements.sh
@@ -1,5 +1,4 @@
 conda install pytorch torchvision cuda92 ignite -c pytorch -y
-conda install tensorflow -c anaconda -y
-pip install --user grpcio msgpack torchtext spacy tensorboardX download
+pip install --user tensorflow grpcio msgpack torchtext spacy tensorboardX download
 python -m spacy download de
 python -m spacy download en
