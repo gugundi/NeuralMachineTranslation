BUG FIX: in gaussian calculation, matched it with the one from article

model.py
@@ -127,9 +127,7 @@ class Attention(nn.Module):
             window_size = window_end - window_start + 1
             h_s[:window_size, i] = h_s_batch[window_start:window_end+1, i]
             positions = torch.arange(window_start, window_end + 1, device=self.device, dtype=torch.float)
-            print('window size: {window_size}')
-            print(torch.exp((positions - p) / (2 * self.std_squared)).shape)
-            gaussian_batch[i, :window_size] = torch.exp((positions - p) / (2 * self.std_squared))
+            gaussian_batch[i, :window_size] = torch.exp(-(positions - p) ** 2 / (2 * self.std_squared))
         h_s_batch = h_s
 
         # batch x window x hidden