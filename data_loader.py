0f3f363e78a3dfc5a2ceaa2450532bdbeb64c0ab
Gabriel <gugundi@gmail.com>
2018-12-10 17:00:41 +0100

    Updated data loader with lod_multi30k function

--- data_loader.py
+++ data_loader.py
@@ -85,26 +85,45 @@ def load_iwslt(config, SOS_token, EOS_token, PAD_token, device):
 
     return train_iter_iwslt, val_iter_iwslt, DE_IWSLT.vocab, EN_IWSLT.vocab, train_iwslt, val_iwslt
 
+def load_multi30k(config, SOS_token, EOS_token, PAD_token, device):
+    print("Started data-loader: Multi30K (de-en)")
 
-"""
-# set up fields for Multi30K
-DE_Multi30K = data.Field(tokenize=tokenize_x('de'))
-EN_Multi30K = data.Field(tokenize=tokenize_x('en'))
+    # set up fields for IWSLT
+    DE_MULTI30K = torchtext.data.Field(
+        tokenize=tokenize_de,
+        init_token=SOS_token,
+        eos_token=EOS_token,
+        pad_token=PAD_token,
+        include_lengths=True
+    )
+    EN_MULTI30K = torchtext.data.Field(
+        tokenize=tokenize_en,
+        init_token=SOS_token,
+        eos_token=EOS_token,
+        pad_token=PAD_token,
+        include_lengths=True
+    )
 
-# make splits for data in Multi30K
-train_multi30k, val_multi30k, test_multi30k = datasets.IWSLT.splits(exts=('.de', '.en'), fields=(DE_Multi30K, EN_Multi30K))
+    print("Making splits for Multi30K")
+    # make splits for data in IWSLT
+    train_multi30k, val_multi30k, test_multi30k = torchtext.datasets.IWSLT.splits(exts=('.de', '.en'), fields=(DE_MULTI30K, EN_MULTI30K))
 
-# build the vocabulary of Multi30K
-DE_Multi30K.build_vocab(train_multi30k.scr, min_freq=3)
-EN_Multi30K.build_vocab(train_multi30k.trg, max_size=20000)
+    print("Building vocabulary for Multi30K")
+    # build the vocabulary of IWSLT
+    source_vocabulary_size = config.get('source_vocabulary_size')
+    target_vocabulary_size = config.get('target_vocabulary_size')
+    DE_MULTI30K.build_vocab(train_iwslt.src, max_size=source_vocabulary_size)
+    EN_MULTI30K.build_vocab(train_iwslt.trg, max_size=target_vocabulary_size)
 
-# make iterator for splits in Multi30K
-train_iter_multi30k, val_iter_multi30k = data.BucketIterator.splits(
-    (train_multi30k, val_multi30k), batch_size=3, device=0)
+    print("Making iterator splits for Multi30K")
+    # make iterator for splits in IWSLT
+    train_iter_multi30k, val_iter_multi30k = torchtext.data.BucketIterator.splits(
+        (train_multi30k, val_multi30k),
+        batch_size=config.get('batch_size'),
+        device=device,
+        shuffle=True
+    )
 
-print(DE_IWSLT.vocab.freqs.most_common(10))
-print(len(DE_IWSLT.vocab))
-print(EN_IWSLT.vocab.freqs.most_common(10))
-print(len(EN_IWSLT.vocab))
+    print("Finished loading Multi30K")
 
-"""
+    return train_iter_multi30k, val_iter_multi30k, DE_MULTI30K.vocab, EN_MULTI30K.vocab, train_multi30k, val_multi30k
